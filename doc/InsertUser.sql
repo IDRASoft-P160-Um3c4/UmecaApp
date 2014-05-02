11942cbf708accf8fc012fc9cfe9d170c35129bb
Israel <israelvi@hotmail.com>
2014-05-02 13:53:11 -0500

    Faltantes
    Faltantes

---
+++ doc/InsertUser.sql
@@ -0,0 +1,78 @@
+insert into user (username, password, enabled) values ("administrador", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into role (role, description) values ("ROLE_ADMIN", "Administradores");
+insert into role (role, description) values ("ROLE_USER", "Usuario");
+insert into user_role (id_user, id_role) values (1,1);
+
+
+
+
+insert into role (role, description) values ("ROLE_USER1", "Usuario1");
+insert into role (role, description) values ("ROLE_USER2", "Usuario2");
+insert into role (role, description) values ("ROLE_USER3", "Usuario3");
+insert into role (role, description) values ("ROLE_USER4", "Usuario4");
+insert into role (role, description) values ("ROLE_USER5", "Usuario5");
+insert into role (role, description) values ("ROLE_USER6", "Usuario6");
+insert into role (role, description) values ("ROLE_USER7", "Usuario7");
+insert into role (role, description) values ("ROLE_USER8", "Usuario8");
+insert into role (role, description) values ("ROLE_USER9", "Usuario9");
+insert into role (role, description) values ("ROLE_USER10", "Usuario10");
+insert into role (role, description) values ("ROLE_USER11", "Usuario11");
+insert into role (role, description) values ("ROLE_USER12", "Usuario12");
+insert into role (role, description) values ("ROLE_USER13", "Usuario13");
+insert into role (role, description) values ("ROLE_USER14", "Usuario14");
+insert into role (role, description) values ("ROLE_USER15", "Usuario15");
+insert into role (role, description) values ("ROLE_USER16", "Usuario16");
+
+
+insert into role (role, description) values ("AAA", "AAA");
+insert into role (role, description) values ("ABB", "BBB");
+insert into role (role, description) values ("BBB", "BBB");
+insert into role (role, description) values ("BCC", "ccc");
+insert into role (role, description) values ("CCC", "CCC");
+insert into role (role, description) values ("CDD", "ddd");
+insert into role (role, description) values ("DDD", "ddd");
+insert into role (role, description) values ("DEE", "eee");
+insert into role (role, description) values ("EEE", "eee");
+insert into role (role, description) values ("EFF", "eee");
+insert into role (role, description) values ("FFF", "fff");
+insert into role (role, description) values ("FGG", "fff");
+insert into role (role, description) values ("GGG", "ggg");
+insert into role (role, description) values ("GHH", "gfg");
+insert into role (role, description) values ("HHH", "hhf");
+insert into role (role, description) values ("HHI", "iii");
+
+
+
+insert into user (username, password, enabled) values ("user aaaa", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user bbbb", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user cccc", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user ccdd", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user dddd", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user eeee", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user eeff", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user ffff", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user gggg", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user hhhh", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user iiii", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user jjjj", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user kkkk", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user llll", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user mmmm", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user nnnn", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user oooo", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user pppp", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user ppqq", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user qqqq", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user qqrr", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user rrrr", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user rrss", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user ssss", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user ssst", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user uuuu", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user vvvv", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user wwww", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user xxxx", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user yyyy", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+insert into user (username, password, enabled) values ("user zzzz", "$2a$10$qLDt3VE0hNlNagllqdl1AOVM3qeOJwhVqsOcpzITBYFYYRHgPYKRm", true);
+
+
