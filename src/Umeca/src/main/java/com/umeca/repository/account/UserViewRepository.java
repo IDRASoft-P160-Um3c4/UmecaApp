4b8041191b880580b7a4ce2dc530fbdfbec136a2
IDRASoft-P160-Um3c4 <adrian.mapj@hotmail.com>
2014-05-21 11:25:40 -0500

    Clases y vistas para Opinión Técnica

--- src/Umeca/src/main/java/com/umeca/repository/account/UserViewRepository.java
+++ src/Umeca/src/main/java/com/umeca/repository/account/UserViewRepository.java
@@ -28,7 +28,7 @@ import java.util.List;
 
 @Repository("userviewRepository")
 public class UserViewRepository {
-
+//sdfsdfsdf
     @PersistenceContext(unitName = "punit")
     private javax.persistence.EntityManager entityManager;
 
