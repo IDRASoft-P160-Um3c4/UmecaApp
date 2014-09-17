1006737f62ded6b1eab70d3a544761ac5d30d0eb
Daniel Cortes Rivera <daniel.cortes@idrasoft.com.mx>
2014-09-17 17:22:22 -0500

    Histórico de mensajes

--- src/Umeca/src/main/java/com/umeca/model/entities/shared/MessageHistoryView.java
+++ src/Umeca/src/main/java/com/umeca/model/entities/shared/MessageHistoryView.java
@@ -26,9 +26,52 @@ public class MessageHistoryView implements EntityGrid {
 
     private String idFolder;
 
+    public String getRequestType() {
+        return requestType;
+    }
 
-    public MessageHistoryView(Long id, String idFolder){
+    public void setRequestType(String requestType) {
+        this.requestType = requestType;
+    }
+
+    private String requestType;
+
+    public String getResponseType() {
+        return responseType;
+    }
+
+    public void setResponseType(String responseType) {
+        this.responseType = responseType;
+    }
+
+    private String responseType;
+
+    public String getSender() {
+        return sender;
+    }
+
+    public void setSender(String sender) {
+        this.sender = sender;
+    }
+
+    private String sender;
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    private String message;
+
+    public MessageHistoryView(Long id, String idFolder, String requestType, String responseType, String sender, String message){
         this.id = id;
         this.idFolder = idFolder;
+        this.requestType = requestType;
+        this.responseType = responseType;
+        this.sender = sender;
+        this.message = message;
     }
 }
