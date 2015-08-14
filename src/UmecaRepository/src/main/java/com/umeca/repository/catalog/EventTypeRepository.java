fd8170f43d9e7477fa23e45048dfb0ba76cdc376
Arturo De La Rosa <keats@live.com.mx>
2015-08-14 12:43:27 -0500

    EventTypeRepository-Fixed event.txt update EventRepository Added -Fixed EventType Repository - Fix Event - Fix Negation - Fix Evaluator - NegationDocument Evaluator - Metting Fixes Coreccion de costructor MeetingView Manager Evaluator, Evaluator - Notifications Update

--- src/UmecaRepository/src/main/java/com/umeca/repository/catalog/EventTypeRepository.java
+++ src/UmecaRepository/src/main/java/com/umeca/repository/catalog/EventTypeRepository.java
@@ -1,7 +1,6 @@
 package com.umeca.repository.catalog;
 
 import com.umeca.model.catalog.EventType;
-import com.umeca.model.catalog.StatusCase;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -11,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository("eventTypeRepository")
 public interface EventTypeRepository extends JpaRepository<EventType,Long> {
 
-    @Query("SELECT s from EventType s where s.name=:code")
-    public StatusCase findByCode(@Param("code") String code);
+    @Query("SELECT e from EventType e where e.name=:code")
+    public EventType findByCode(@Param("code") String code);
+
 }
