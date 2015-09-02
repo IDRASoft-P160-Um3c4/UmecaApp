dba1d2990801d68f9290ed2d6b08bf00a49327a2
Israel Vite Silva <israelvi@hotmail.com>
2015-09-02 16:08:13 -0500

    UM Director 026

--- src/Umeca/src/main/java/com/umeca/service/shared/SharedLogExceptionService.java
+++ src/Umeca/src/main/java/com/umeca/service/shared/SharedLogExceptionService.java
@@ -7,13 +7,6 @@ import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-/**
- * Project: Umeca
- * User: Israel
- * Date: 6/14/14
- * Time: 10:53 AM
- */
-
 @Service
 public class SharedLogExceptionService {
 
