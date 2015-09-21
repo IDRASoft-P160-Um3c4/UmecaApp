33ce06ce72559db339b9f3ae687a091e923e851e
Arturo De La Rosa <keats@live.com.mx>
2015-09-21 12:07:55 -0500

    ManagerSup- Reporte 5

--- src/Umeca/src/main/java/com/umeca/service/supervisiorManager/StatisticSupervisorManagerReportServiceImpl.java
+++ src/Umeca/src/main/java/com/umeca/service/supervisiorManager/StatisticSupervisorManagerReportServiceImpl.java
@@ -1,24 +1,25 @@
 package com.umeca.service.supervisiorManager;
 
 import com.google.gson.Gson;
+import com.umeca.model.catalog.DrugType;
 import com.umeca.model.shared.Constants;
 import com.umeca.model.shared.ReportList;
 import com.umeca.model.shared.SelectList;
 import com.umeca.repository.EventRepository;
 import com.umeca.repository.account.UserRepository;
 import com.umeca.repository.catalog.ArrangementRepository;
+import com.umeca.repository.catalog.DrugTypeRepository;
 import com.umeca.repository.catalog.ReportTypeRepository;
-import com.umeca.repository.shared.ReportExcelRepository;
 import com.umeca.repository.supervisorManager.StatisticSupervisorManagerReportRepository;
 import com.umeca.service.account.SharedUserService;
 import com.umeca.service.shared.SharedLogExceptionService;
-import org.hibernate.sql.Select;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.List;
 
@@ -50,6 +51,10 @@ public class StatisticSupervisorManagerReportServiceImpl implements StatisticSup
 
     @Autowired
     UserRepository userRepository;
+
+
+    @Autowired
+    DrugTypeRepository drugTypeRepository;
 
 
     @Override
@@ -195,7 +200,81 @@ public class StatisticSupervisorManagerReportServiceImpl implements StatisticSup
 
                     break;
             }
+            case Constants.REPORT_STATISTIC_MANAGER_REPORT_E:
+                switch (reportTypeRepository.getReportCodeById(idReportType)){
+                    case Constants.REPORT_STATISTIC_MANAGER_GENERAL:
+                        lstObjects = statisticSupervisorManagerReportRepository.countTypeofDrugs(initDateF, endDateF);
+                        for (Object object : lstObjects) {
+                            Object[] aux = (Object[]) object;
+                            data.add(new SelectList(aux[0].toString(), Long.parseLong(aux[1].toString())));
+                        }
+
+                        return gson.toJson(data);
+
+                    case Constants.REPORT_STATISTIC_MANAGER_BY_DISTRICT:
+                        lstObjects = statisticSupervisorManagerReportRepository.countTypeofDrugsByDistrict(initDateF, endDateF, idDistrict);
+                        for (Object object : lstObjects) {
+                            Object[] aux = (Object[]) object;
+                            data.add(new SelectList(aux[0].toString(), Long.parseLong(aux[1].toString())));
+                        }
+
+                        return gson.toJson(data);
+
+                    case Constants.REPORT_STATISTIC_MANAGER_BY_OPERATOR:
+                        List<SelectList> users = userRepository.getLstValidUsersByRole(Constants.ROLE_SUPERVISOR);
+                        List<Object> dataEnd = new ArrayList<>();
+                        List<DrugType> drugs =  drugTypeRepository.findNotObsoleteImportant();
+                        int x = 0;
+                        for(SelectList u : users){
+                            lstObjects = statisticSupervisorManagerReportRepository.countTypeofDrugsByDistrictAndSupervisor(initDateF, endDateF, idDistrict, u.getId());
+                            dataEnd = completeData(dataEnd, lstObjects, u.getName(), x, drugs);
+                            x += 1;
+                        }
+                        return gson.toJson(dataEnd);
+                }
         }
         return null;
+    }
+
+    private List<Object> completeData(List<Object> finalData, List<Object> data, String supervisor, int x, List<DrugType> drugs) {
+
+
+        int countNum = 0;
+
+
+        for(int i = 0; i < drugs.size(); i++) {
+            List<ReportList> aux = new ArrayList<>();
+            if(finalData.size() > i){
+                aux = (List<ReportList>) finalData.get(i);
+            }
+            if(data.size() == 0){
+                aux.add(new ReportList(new Long(x), new Long(0), drugs.get(i).getName(), supervisor, (long) x));
+
+            }else{
+                for(int j = countNum; j < data.size(); j++){
+                    Object[] obj = (Object[]) data.get(j);
+
+                    if(drugs.get(i).getId() == Long.parseLong(obj[0].toString())){
+                        aux.add(new ReportList(new Long(x), Long.parseLong(obj[2].toString()), drugs.get(i).getName(), supervisor, (long) x));
+                    }else {
+                        aux.add(new ReportList(new Long(x), new Long(0), drugs.get(i).getName(), supervisor, (long) x));
+                    }
+                    countNum = j + 1;
+                    break;
+
+
+                }
+            }
+
+            if(finalData.size() <= i){
+                finalData.add(aux);
+            }else{
+                finalData.set(i, aux);
+            }
+
+        }
+
+        return finalData;
+
     }
 }
--- src/UmecaRepository/src/main/java/com/umeca/repository/supervisorManager/StatisticSupervisorManagerReportRepository.java
+++ src/UmecaRepository/src/main/java/com/umeca/repository/supervisorManager/StatisticSupervisorManagerReportRepository.java
@@ -1,7 +1,5 @@
 package com.umeca.repository.supervisorManager;
 
-import com.umeca.model.catalog.Question;
-import com.umeca.model.catalog.StatisticReportType;
 import com.umeca.model.catalog.StatisticSupervisorManagerReportType;
 import com.umeca.model.shared.SelectList;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -9,6 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -79,4 +78,61 @@ public interface StatisticSupervisorManagerReportRepository  extends JpaReposito
             "where hearing_format.id_user = :supervisorId and assigned_arrangement.id_arrangement = :arrangementId and hearing_format.id_district = :districtId " +
             "group by assigned_arrangement.id_arrangement",  nativeQuery = true)
     List<Object> getArrangementByIdAndSupervisorId(@Param("supervisorId") Long supervisorId, @Param("arrangementId") Long arrangementId, @Param("districtId") Long districtId);
+
+
+
+
+    //Sustancias general
+    @Query(value = "select drug, count(idDrug) " +
+            "from (select distinct FM.id_framing_meeting as idFr, DR.id_drug_type as idDrug, DRTY.drug as drug " +
+            "from case_detention CA " +
+            "inner join framing_meeting FM on CA.id_case=FM.id_case " +
+            "inner join drug DR on FM.id_framing_meeting=DR.id_framing_meeting " +
+            "inner join cat_drug_type DRTY on DR.id_drug_type=DRTY.id_drug_type " +
+            "where DRTY.id_drug_type<>15 " +
+            "and FM.end_date between :initDate and :endDate " +
+            "and FM.is_terminated = true) as t1 " +
+            "group by drug " +
+            "order by drug ", nativeQuery = true)
+    List<Object> countTypeofDrugs(@Param("initDate") Date initDate, @Param("endDate") Date endDate);
+
+
+    //Sustancias por distrito
+    @Query(value = "select drug, count(idDrug) " +
+            "from (select distinct FM.id_framing_meeting as idFr, DR.id_drug_type as idDrug, DRTY.drug as drug " +
+            "from case_detention CA " +
+            "inner join framing_meeting FM on CA.id_case=FM.id_case " +
+            "inner join drug DR on FM.id_framing_meeting=DR.id_framing_meeting " +
+            "inner join cat_drug_type DRTY on DR.id_drug_type=DRTY.id_drug_type " +
+            "inner join cat_district DIST on CA.id_district=DIST.id_district " +
+            "where DRTY.id_drug_type<>15 " +
+            "and FM.end_date between :initDate and :endDate " +
+            "and DIST.id_district = :districtId " +
+            "and FM.is_terminated = true) as t1 " +
+            "group by drug " +
+            "order by drug ", nativeQuery = true)
+    List<Object> countTypeofDrugsByDistrict(@Param("initDate") Date initDate, @Param("endDate") Date endDate, @Param("districtId") Long districtId);
+
+
+    //Sustancias por distrito Y operador
+    @Query(value = " select idDrug, drug, count(idDrug), user " +
+            "from (select distinct FM.id_framing_meeting as idFr, DR.id_drug_type as idDrug, DRTY.drug as drug, USR.fullname as user " +
+            "from case_detention CA " +
+            "inner join framing_meeting FM on CA.id_case=FM.id_case " +
+            "inner join drug DR on FM.id_framing_meeting = DR.id_framing_meeting " +
+            "inner join cat_drug_type DRTY on DR.id_drug_type = DRTY.id_drug_type " +
+            "inner join cat_district DIST on CA.id_district = DIST.id_district " +
+            "inner join user USR on FM.id_user = USR.id_user " +
+            "where DRTY.id_drug_type<>15 " +
+            "and FM.id_user = :supervisorId " +
+            "and FM.end_date between :initDate and :endDate " +
+            "and DIST.id_district = :districtId " +
+            "and FM.is_terminated = true) as t1 " +
+            "group by drug " +
+            "order by idDrug ", nativeQuery = true)
+    List<Object> countTypeofDrugsByDistrictAndSupervisor(@Param("initDate") Date initDate, @Param("endDate") Date endDate, @Param("districtId") Long districtId, @Param("supervisorId") Long supervisorId);
+
+
+
+
 }
